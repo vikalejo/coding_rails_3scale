a9d56bc5c326b4c789ae987d97f52b811c8b1d05
Wojciech Ogrodowczyk <wojciech@haikuco.de>
2015-07-29 15:46:17 +0200

    Add products

--- Gemfile
+++ Gemfile
@@ -36,5 +36,6 @@ group :development, :test do
   # Access an IRB console on exception pages or by using <%= console %> in views
   gem 'web-console', '~> 2.0'
   gem 'rspec-rails', '~> 3.0'
+  gem 'faker', '~> 1.4.3'
 end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Product.destroy_all
+
+20.times do
+  Product.create! name: Faker::Commerce.product_name,
+    price_in_cents: Faker::Commerce.price * 100,
+    description: Faker::Lorem.paragraph
+end
+
