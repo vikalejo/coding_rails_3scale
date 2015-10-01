219820949b78c545b5103e1927ba7718652909b8
vikalejo <victor.alejo@wuaki.tv>
2015-10-01 20:21:53 +0200

    Allow Model-Controller to use currency
    - Modify controller index and show sending currency
    - Save a cookie currency in the session
    - Modify model to build new objects in memory with new currency
    - Update details view of product to use a dinamic currency

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,37 @@
 class Product < ActiveRecord::Base
+  attr_accessor :currency
+  attr_accessor :price
 
   def price
     price_in_cents / 100.0
+  end
+
+  # Use the currency selected to return the products
+  # using the correct rate
+  # It does not modify the ddbb value
+  def self.all_by_currency(rate = nil, currency)
+    rate = 1 if currency == Rate.default_currency
+    rate ||= Rate.get_exchange_rates(currency)
+
+    Product.all.map do |product|
+      attributes = product.attributes
+      attributes['price_in_cents'] = attributes['price_in_cents'] * rate
+
+      Product.new(attributes).tap do |p|
+        p.currency = currency
+      end
+    end
+  end
+
+  # Using the currency selected, returns the product with
+  # the correct rate in the product description
+  def self.find_by(params)
+    product = Product.where(id: params['id']).first
+    attributes = product.attributes
+
+    Product.new(attributes).tap do |p|
+      p.price = params['price'].to_i
+      p.currency = params['currency']
+    end
   end
 end
